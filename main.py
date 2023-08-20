41cfdfc301d230f43b366871b24f83bd70961b8e
jkoren <j.korenstein@gmail.com>
2023-08-19 21:31:27 -0400

    added write to csv

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # https://www.freecodecamp.org/news/loading-a-json-file-in-python-how-to-read-and-parse-json/
 # https://www.w3schools.com/python/python_for_loops.asp
+# https://www.scaler.com/topics/how-to-create-a-csv-file-in-python/
 
 import json
+import csv
 
 with open('qa-groups-roles.json') as groups_roles_file:
   groups_roles_contents = groups_roles_file.read()
@@ -21,11 +23,23 @@ parsed_roles_entitlements = json.loads(roles_entitlements_contents)
 # print
 
 roles = parsed_roles_entitlements["roles"]
-for role in roles:
-  print("Role:",role["roleName"])
-  entitlements = role["entitlements"]
-  for entitlement in entitlements:
-    print(" Role: ",role["roleName"], " Entitlement:", entitlement)
-    # find the groups for this role from Groups - will need a filter on groups to find?
-    # print out one line for each Entitlement, Role and Group
-    # print(" Role: ",role["roleName"], " Entitlement:", entitlement, "Group: ")
+with open('groups-roles-entitlements.csv', 'w', newline='') as file:
+  writer = csv.writer(file)
+  writer.writerow(["Role", "Group", "Platform", "Tenant", "Case Type", "Case Subtype", "Entitlement"])
+
+  for role in roles:
+    print("Role:",role["roleName"])
+    entitlements = role["entitlements"]
+    for entitlement in entitlements:
+      # example entitlement = "omnius_case|complaints_case|tier_2|complaint_servicing|create_case"
+      entitlementParts = entitlement.split('|')
+      platform = entitlementParts[0]
+      tenant = entitlementParts[1]
+      caseType = entitlementParts[2]
+      caseSubtype = entitlementParts[3]
+      theEntitlement = entitlementParts[4]
+
+      print(" Role: ",role["roleName"], " Entitlement:", entitlement)
+      # find the groups for this role from Groups - will need a filter on groups to find?
+      # print out one line for each Entitlement, Role and Group
+      writer.writerow([role["roleName"], "this is the group", platform, tenant, caseType, caseSubtype, theEntitlement])
