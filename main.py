31e01d262b74a8acc4bf86c53c6a9257fa0f0b67
jkoren <j.korenstein@gmail.com>
2023-08-20 20:56:09 -0400

    refactored to process in order of groups - to make output easier to understand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # https://www.freecodecamp.org/news/loading-a-json-file-in-python-how-to-read-and-parse-json/
 # https://www.w3schools.com/python/python_for_loops.asp
 # https://www.scaler.com/topics/how-to-create-a-csv-file-in-python/
+# https://stackoverflow.com/questions/598398/searching-a-list-of-objects-in-python
 
 import json
 import csv
@@ -13,27 +14,28 @@ with open('qa-roles-entitlements.json') as roles_entitlements_file:
 
 parsed_groups_roles = json.loads(groups_roles_contents)
 parsed_roles_entitlements = json.loads(roles_entitlements_contents)
-groups = parsed_groups_roles["groups"]
-roles = parsed_roles_entitlements["roles"]
+allGroups = parsed_groups_roles["groups"]
+allRoles = parsed_roles_entitlements["roles"]
 
 with open('groups-roles-entitlements.csv', 'w', newline='') as file:
   writer = csv.writer(file)
   writer.writerow(["Group", "Role", "Platform", "Tenant", "Case Type", "Case Subtype", "Entitlement"])
 
-  for role in roles:
-    roleName = role["roleName"]
-    print("Role:",roleName)
-    entitlements = role["entitlements"]
-    for entitlement in entitlements:
-      entitlementParts = entitlement.split('|')
-      platform = entitlementParts[0]
-      tenant = entitlementParts[1]
-      caseType = entitlementParts[2]
-      caseSubtype = entitlementParts[3]
-      theEntitlement = entitlementParts[4]
-
-      groupsForThisRole = [group for group in groups if roleName in group["roles"]]
-
-      for group in groupsForThisRole:
-        print(group["groupName"], role["roleName"], platform, tenant, caseType, caseSubtype, theEntitlement)
-        writer.writerow([group["groupName"], role["roleName"], platform, tenant, caseType, caseSubtype, theEntitlement])
+  for group in allGroups:
+    groupName = group["groupName"]
+    roles = group["roles"]
+    print("Group: ",groupName)
+    for role in roles:
+      print(" Role: ",role)
+      roleEntitlements = next((roleEntitlement for roleEntitlement in allRoles if roleEntitlement["roleName"] == role), None)
+      entitlements = roleEntitlements["entitlements"]
+      for entitlement in entitlements:
+        print("  Entitlement: ",entitlement)
+        entitlementParts = entitlement.split('|')
+        platform = entitlementParts[0]
+        tenant = entitlementParts[1]
+        caseType = entitlementParts[2]
+        caseSubtype = entitlementParts[3]
+        theEntitlement = entitlementParts[4]
+        # print(groupName, role, platform, tenant, caseType, caseSubtype, theEntitlement)
+        writer.writerow([groupName, role, platform, tenant, caseType, caseSubtype, theEntitlement])
